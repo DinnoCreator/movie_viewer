432edbd9d316b6b742b14599e7ec048b62ec4aa7
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2023-02-07 20:22:12 +0100

    Update Feed3.js

--- src/pages/Feed3.js
+++ src/pages/Feed3.js
@@ -66,7 +66,7 @@ const Feed3 = () => {
         <>
           <div className="container">
           <div className="row container" style={{textAlign: "center"}}>
-            <h2 className="mt-3">Random Movies page 1</h2>
+            <h2 className="mt-3">Random Movies page 3</h2>
           {movieShow}
           </div>
           </div>
