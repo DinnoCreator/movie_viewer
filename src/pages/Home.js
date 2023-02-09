bbaab65197d7d39865463857a46ea6183ad11e10
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2023-02-09 16:31:09 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const Home = () => {
   const [error, setError] = useState("A new QrCode would be generated in");
   const [qrcode, setQrcode] = useState("");
+  const [loading, setLoading] = useState(false);
 
   let [timer, setTimer] = useState(10);
 
@@ -15,6 +16,7 @@ const Home = () => {
   }, 1000);
 
   const getQrcode = useCallback(async () => {
+    setLoading(true);
     try {
       await fetch(`https://nest-api-vxd3.onrender.com/qrcode`, {
         method: "GET",
@@ -29,6 +31,7 @@ const Home = () => {
         })
         .then(function (jsonData) {
           setQrcode(jsonData.qrcode);
+          setLoading(false);
         });
       // await axios.get('http://localhost:3333/qrcode')
       //   .then(function (response) {
@@ -43,13 +46,31 @@ const Home = () => {
     getQrcode();
   }, [getQrcode]);
 
+  const show = () => {
+    if (loading)
+      return (
+      <>
+        <div className="center">
+        <div></div>
+        <div
+          style={{ display: "inline-block" }}
+          className="loaderBig"
+        ></div>
+      </div>
+      </>
+    );
+
+     return (
+        <img src={qrcode} alt="qr" />
+       );
+}
   return (
     <>
       <div className="container mt-5">
       <div className="container" style={{textAlign: "center"}}>
         <h1>{error}</h1>
         <h1>{timer} sec</h1>
-        <img src={qrcode} alt="qr" />
+        {show()}
       </div>
       </div>
     </>
